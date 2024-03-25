730adabc795820d1ab9fa9ee54acb62ee311ee02
pxeger <_@pxeger.com>
2024-03-25 22:01:23 +0000

    fix image pushing in autogenerated CI config

--- gen_ci_config.py
+++ gen_ci_config.py
@@ -11,21 +11,22 @@ for dockerfile in glob.iglob("./**/Dockerfile", recursive=True):
     assert re.fullmatch(r"[\w-]+", name), f"unsafe name {name!r}"
     with open(dockerfile) as f:
         dependencies = [m.group(1) for m in re.finditer(r"^\s*FROM\s+attemptthisonline/(\S+).*$", f.read(), re.M)]
-    tags = [
-        tag_base + name + ":" + tag
+    build_tag = "attemptthisonline/" + name
+    push_tags = [
+        tag_base + name + ":" + version
         for tag_base in (
             "registry.gitlab.pxeger.com/attempt-this-online/languages/",
-            "docker.io/attemptthisonline/",
+            # "docker.io/attemptthisonline/",  # TODO: login to Docker Hub
         )
-        for tag in ("$now", "latest")
+        for version in ("$now", "latest")
     ]
     result[f"build {name}"] = {
         "extends": ".base",  # `.build` template is defined in main YAML file
         "needs": [f"build {dep}" for dep in dependencies] or ["init"],
         "script": [
             "now=$(echo $CI_PIPELINE_CREATED_AT | tr T: - | head -c 19)",
-            "podman build --no-cache " + context + "".join(" -t " + t for t in tags),
-            "podman push " + " ".join(tags),
+            f"podman build --no-cache {context} -t {build_tag}",
+            *[f"podman push {build_tag} {push_tag}" for push_tag in push_tags],
         ],
     }
 
